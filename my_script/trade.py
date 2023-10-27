b334948b543954db9f3d9f090304c34da71a8575
酷酷的名字 <qzh98mail@163.com>
2023-10-27 15:23:21 +0800

    结算业务编写中

--- my_script/trade.py
+++ my_script/trade.py
@@ -7,7 +7,7 @@ import json
 import get_token
 import dependencies as dd
 from common import logging
-class InfoSimpleCart:
+class Trade:
     log = logging.myLogger()
     def __init__(self):
         # 获得登录的ticket和token
@@ -20,8 +20,8 @@ class InfoSimpleCart:
         self.headers["ticket"] = self.ticket
         self.headers["token"] = self.token
 
-    def info_simple_cart(self):
-        path = "/api/single/shopcart/infoSimpleCart"
+    def refresh_trade_info(self):
+        path = "/api/single/trade/refreshTradeInfo"
         url = self.url + path
         params = []
         params = json.dumps(params)
@@ -29,15 +29,3 @@ class InfoSimpleCart:
         self.log.debug((f'初始化购物车请求地址:{url}  请求体:{params}  请求头:{self.headers}'))
         response = dd.requests.post(url=url,headers=self.headers,data=params).json()
         self.log.info('响应结果{}'.format(response))
-        if response["code"] == '0000':
-            simpleWareRpcRespList = response["data"][0]["simpleWareRpcRespList"]
-            self.log.debug("提取到购物车商品列表{}".format(simpleWareRpcRespList))
-            return simpleWareRpcRespList
-        else:
-            self.log.debug("购物车初始化失败，错误信息{}".format(response))
-
-
-
-if __name__ == '__main__':
-    shop = InfoSimpleCart()
-    shop.info_simple_cart()
