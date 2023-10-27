81c548e4836aa48193a3af1b0e0a20b7c312ced9
酷酷的名字 <qzh98mail@163.com>
2023-10-27 13:31:41 +0800

    修改初始化购物车方法

--- my_script/add_cart.py
+++ my_script/add_cart.py
@@ -26,9 +26,10 @@ class  ShopCart:
         self.headers = get_token.base_headers
         self.headers["ticket"] = self.ticket
         self.headers["token"] = self.token
+        self.__add_cart()  # 在初始化时直接调用 __add_cart 方法
 
     # @ddt.file_data("../data/SKU.yaml")
-    def add_cart(self,**kwargs):
+    def __add_cart(self,**kwargs):
         path = "/api/single/shopcart/addCart"
         url = self.url + path
 
@@ -50,7 +51,30 @@ class  ShopCart:
             else:
                 self.log.debug("商品加购失败，错误信息{}".format(response))
 
+    def info_simple_cart(self):
+        if not self.__add_cart():
+            self.__add_cart() # 初始化购物车之前检查商品加购没有，没有就先调用加车方法
+
+        path = "/api/single/shopcart/infoSimpleCart"
+        url = self.url + path
+        params = []
+        params = json.dumps(params)
+        params = {"params":params}
+        self.log.debug((f'初始化购物车请求地址:{url}  请求体:{params}  请求头:{self.headers}'))
+        response = dd.requests.post(url=url,headers=self.headers,data=params).json()
+        self.log.info('响应结果{}'.format(response))
+        if response["code"] == '0000':
+            simpleWareRpcRespList = response["data"][0]["simpleWareRpcRespList"]
+            self.log.debug("提取到购物车商品列表{}".format(simpleWareRpcRespList))
+            for item in simpleWareRpcRespList:
+                skuId = item["skuId"]
+                count = item["count"]
+                self.log.debug(f"购物车中skuId：{skuId}，加购数量：{count}")
+            return simpleWareRpcRespList
+        else:
+            self.log.debug("购物车初始化失败，错误信息{}".format(response))
 
 if __name__ == '__main__':
     shop = ShopCart()
-    shop.add_cart()
+    # shop.add_cart()
+    shop.info_simple_cart()
